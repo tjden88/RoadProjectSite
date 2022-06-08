49b8f22544b6c598ae3125bbd5729757f63a06f4
Денис <dultsevdv@gmail.com>
2022-06-09 01:20:51 +0500

    Первичная маршрутизация

--- RoadProjectSite/Program.cs
+++ RoadProjectSite/Program.cs
@@ -1,9 +1,31 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Добавление сервисов
+#region Services
+
+var services = builder.Services;
+services.AddMvc();
+
+#endregion
+
+// Построение приложения
 var app = builder.Build();
+
+// Настройка приложения и маршрутизации
+#region App
+
+if (app.Environment.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+app.UseRouting();
+
+app.MapDefaultControllerRoute();
+
+#endregion
+
+
 
 // Файл конфигурации
 var appCfg = app.Configuration;
-
-app.MapGet("/", () => appCfg["CompanyName"]);
+app.MapGet("/debug", () => appCfg["CompanyName"]);
 
 app.Run();
