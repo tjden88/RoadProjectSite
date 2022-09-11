9eea370424aa70746fdf5b00719a24af7afd239e
Денис <dultsevdv@gmail.com>
2022-09-12 02:59:23 +0500

    HomeController и разметка домашней страницы

---
+++ RoadProjectSite/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoadProjectSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
